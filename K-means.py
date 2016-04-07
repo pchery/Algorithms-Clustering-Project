f146b1477f6c1884bae738170e956fd0bd7b845e
Paul Chery <paulachery@gmail.com>
2016-04-06 20:31:38 -0500

    Initial commit

---
+++ K-means.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+def kmeans(A):
+
+    #get random centroids
+
+    #repeat{
+    #assign all data points to a centroid that is closest to it
+
+    #move centroids to average location of points closest to it
