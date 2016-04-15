14f2227faea2ecf5b2fdbb6e18cadc28da904859
Paul Chery <paulachery@gmail.com>
2016-04-15 11:29:55 -0500

    started implementing helper functions

--- K-means.py
+++ K-means.py
@@ -1,13 +1,43 @@
 
+import random
 
 
 
+# def kmeans(A, k):
+#
+#     Forgy method: randomly choose k observations as centroids
+#     Randome partitioning: randomly assigns a cluster to each observation
+#     #get random centroids
+#
+#     #repeat{
+#     #assign all data points to a centroid that is closest to it
+#
+#     #move centroids to average location of points closest to it
+# return centroid
 
-def kmeans(A):
+def distance(x,y, a, b):
+    dx = x - a
+    dy = y - b
+    distance = ((dx**2) + (dy**2))**0.5
+    return distance
 
-    #get random centroids
+print distance(1,5,2,6)
 
-    #repeat{
-    #assign all data points to a centroid that is closest to it
+def averageLocation(A):
+    sumX = sumY =  0
+    for (x,y) in A:
+        sumX += x
+        sumY += y
+    averageX = sumX / len(A)
+    averageY = sumY / len(A)
+    return averageX ,averageY
 
-    #move centroids to average location of points closest to it
+
+def generateCentroids(k, s):
+    A = []
+    for i in (0 , k):
+        A[i] = (int(random.random() * s), int(random.random() * s))
+
+
+print averageLocation([(1,2), (1,1)])
+print generateCentroids(3, 5)
