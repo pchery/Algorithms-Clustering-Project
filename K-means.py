3f744e4fb4263fae189e64cbfd03249c57d0585a
Paul Chery <paulachery@gmail.com>
2016-04-16 14:00:26 -0500

    average location function and centroid function

--- K-means.py
+++ K-means.py
@@ -3,8 +3,9 @@ import random
 
 
 
-# def kmeans(A, k):
-#
+#def kmeans(A, k):
+
+
 #     Forgy method: randomly choose k observations as centroids
 #     Randome partitioning: randomly assigns a cluster to each observation
 #     #get random centroids
@@ -21,23 +22,44 @@ def distance(x,y, a, b):
     distance = ((dx**2) + (dy**2))**0.5
     return distance
 
-print distance(1,5,2,6)
-
-def averageLocation(A):
-    sumX = sumY =  0
-    for (x,y) in A:
-        sumX += x
-        sumY += y
-    averageX = sumX / len(A)
-    averageY = sumY / len(A)
-    return averageX ,averageY
+def distanceD(d, A,B):
+    distance = 0
+    for i in range(0,d):
+        distance += (A[i] - B[i])**2
+    return distance**0.5
 
 
-def generateCentroids(k, s):
-    A = []
-    for i in (0 , k):
-        A[i] = (int(random.random() * s), int(random.random() * s))
+#print distance(1,5,2,6)
+
+def averageLocation(d, matrix):
+    B = [0] * d
+    for vector in matrix:
+        for i in range(0, d):
+            B[i] += vector[i]
+    for k in range(0, d):
+        B[k] /= float(len(matrix))
+    return B
 
 
-print averageLocation([(1,2), (1,1)])
-print generateCentroids(3, 5)
+def generateCentroids(k, matrix):
+    centroids = []
+    d = len(matrix)
+    centroidVectors = []
+    for i in range(0, d):
+            centroidVectors.append(matrix[i])
+            if ((i % (d / k) == 0 and i != 0) or (i == d)):
+                centroids.append(averageLocation(3,centroidVectors))
+                centroidVectors = []
+            #random.randint(0, d)
+            d = len(matrix)
+    return centroids
+
+#print averageLocation([(1,2), (1,1)])
+#print generateCentroids(3, 5)
+print distanceD(2, [0,0], [1,0])
+print distanceD(2, [0,0], [1,1])
+print distanceD(3, [0,0,0], [1,1,1])
+
+print averageLocation(3, [[0,0,0],[1,1,1], [1,1,1]])
+
+print generateCentroids(2, [[1,1,1],[2,2,2]])
